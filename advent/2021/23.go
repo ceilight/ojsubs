7da5ff46f8bd4bba3d9a8993894d5ae5cc169b09
geckoil <73975841+geckoil@users.noreply.github.com>
2023-08-12 00:00:03 +0700

    small fix on aoc 2021 day 23 solution

--- advent/2021/23.go
+++ advent/2021/23.go
@@ -5,18 +5,8 @@ import (
 	"container/heap"
 	"fmt"
 	"os"
+	"time"
 )
-
-type Amphipod struct {
-	roomIndex int
-	kind      rune
-}
-
-type State struct {
-	amphipods [16]Amphipod
-	cost      int
-	count     int
-}
 
 var costs = map[rune]int{
 	'A': 1,
@@ -32,15 +22,17 @@ var destX = map[rune]int{
 	'D': 8,
 }
 
-func (s *State) key() string {
-	rooms := make([]rune, 23)
-	for i := range rooms {
-		rooms[i] = '.'
-	}
-	for _, a := range s.amphipods[:s.count] {
-		rooms[a.roomIndex] = a.kind
-	}
-	return string(rooms)
+// there are 23 valid cells, so each cell gets indexed
+var cellX = [23]int{0, 1, 3, 5, 7, 9, 10, 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6, 8, 8, 8, 8}
+var cellY = [23]int{0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4}
+
+type State struct {
+	// keeps track on the state of the 23 cells
+	// for each char indexed by a corresponding cell order, A, B, C, D indicate the amphipods
+	// while the period mark indicates empty cell
+	cells string
+	// the cost of a new state after one amphipod's move
+	cost int
 }
 
 func abs(a int) int {
@@ -50,15 +42,24 @@ func abs(a int) int {
 	return a
 }
 
-func (s State) move(idx int, destX, destY int) *State {
-	a := s.amphipods[idx]
-	x, y := roomX[a.roomIndex], roomY[a.roomIndex]
-	moveD := abs(x-destX) + abs(y-destY)
-	moveCost := moveD * costs[a.kind]
+// returns a new state after an amphipod moves
+func (s *State) move(index int, destX, destY int) State {
+	cellsTemp := []rune(s.cells)
+	kind := cellsTemp[index]
 
-	s.cost = moveCost
-	s.amphipods[idx].roomIndex = getRoomIndex(destX, destY)
-	return &s
+	x, y := cellX[index], cellY[index]
+	distance := abs(x-destX) + abs(y-destY)
+	cost := distance * costs[kind]
+
+	destIndex := getRoomIndex(destX, destY)
+	cellsTemp[destIndex] = kind
+	cellsTemp[index] = '.'
+
+	state := State{
+		cells: string(cellsTemp),
+		cost:  cost,
+	}
+	return state
 }
 
 func (s *State) isHallwayOpen(start, stop int, selfIdx int) bool {
@@ -68,63 +69,61 @@ func (s *State) isHallwayOpen(start, stop int, selfIdx int) bool {
 	} else {
 		xl, xh = start, stop
 	}
-	for i, a := range s.amphipods[:s.count] {
-		x, y := roomX[a.roomIndex], roomY[a.roomIndex]
-		if i != selfIdx && y == 0 && x >= xl && x <= xh {
-			return false
+	for i, r := range s.cells {
+		if r != '.' {
+			x, y := cellX[i], cellY[i]
+			if i != selfIdx && y == 0 && x >= xl && x <= xh {
+				return false
+			}
 		}
 	}
 	return true
 }
 
-func parseState(key string) State {
-	if len(key) != 23 {
-		panic("invalid state key, must have 23 chars")
-	}
-	s := State{}
-	i := 0
-	for roomIndex, a := range key {
-		if a != '.' {
-			s.amphipods[i] = Amphipod{roomIndex, a}
-			i++
-		}
-	}
-	s.count = i
-	return s
-}
-
-var roomX = [23]int{0, 1, 3, 5, 7, 9, 10, 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6, 8, 8, 8, 8}
-var roomY = [23]int{0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4}
-
 func getRoomIndex(x, y int) int {
 	for i := 0; i < 23; i++ {
-		if roomX[i] == x && roomY[i] == y {
+		if cellX[i] == x && cellY[i] == y {
 			return i
 		}
 	}
 	return -1
 }
 
-func getKind(key string, x, y int) (rune, bool) {
+func (s *State) amphipodKind(x, y int) (rune, bool) {
 	index := getRoomIndex(x, y)
 	if index == -1 {
-		return '.', false
+		return '#', false
 	}
-	return []rune(key)[index], true
+	return []rune(s.cells)[index], true
 }
 
-func (s *State) nextStates() []*State {
-	var next []*State
-	key := s.key()
+func (s *State) amphipodCount() int {
+	count := 0
+	for _, r := range s.cells {
+		if r != '.' {
+			count++
+		}
+	}
+	return count
+}
+
+func (s *State) nextStates() []State {
+	next := []State{}
 
 	maxY := 4
-	if s.count == 8 {
+	if s.amphipodCount() == 8 {
 		maxY = 2
 	}
 
-	for i, a := range s.amphipods[:s.count] {
-		ax, ay := roomX[a.roomIndex], roomY[a.roomIndex]
-		dx := destX[a.kind]
+	for i, r := range s.cells {
+		// ignore empty cells
+		if r == '.' {
+			continue
+		}
+
+		ax, ay := cellX[i], cellY[i]
+		dx := destX[r]
+		// already in its right place
 		if ay == maxY && ax == dx {
 			continue
 		}
@@ -132,12 +131,12 @@ func (s *State) nextStates() []*State {
 		if ay == 0 {
 			dy := -1
 			for y := maxY; y >= 1; y-- {
-				kind, found := getKind(key, dx, y)
+				kind, found := s.amphipodKind(dx, y)
 				if !found || kind == '.' {
 					dy = y
 					break
 				}
-				if kind != a.kind {
+				if kind != r {
 					break
 				}
 			}
@@ -153,7 +152,7 @@ func (s *State) nextStates() []*State {
 
 		isStuck := false
 		for y := 1; y < ay; y++ {
-			kind, found := getKind(key, ax, y)
+			kind, found := s.amphipodKind(ax, y)
 			if found && kind != '.' {
 				isStuck = true
 				break
@@ -166,8 +165,8 @@ func (s *State) nextStates() []*State {
 		if ax == dx {
 			isSet := true
 			for y := ay + 1; y <= maxY; y++ {
-				kind, found := getKind(key, ax, y)
-				if !found || kind != a.kind {
+				kind, found := s.amphipodKind(ax, y)
+				if !found || kind != r {
 					isSet = false
 					break
 				}
@@ -177,6 +176,7 @@ func (s *State) nextStates() []*State {
 			}
 		}
 
+		// hallway transition
 		for _, x := range []int{0, 1, 3, 5, 7, 9, 10} {
 			if s.isHallwayOpen(ax, x, i) {
 				next = append(next, s.move(i, x, 0))
@@ -186,20 +186,13 @@ func (s *State) nextStates() []*State {
 	return next
 }
 
-type NodeWithCost struct {
-	node string
-	cost int
-}
-
-type PQueue []NodeWithCost
+// binary heap implementation
+type PQueue []State
 
 func (h PQueue) Len() int           { return len(h) }
 func (h PQueue) Less(i, j int) bool { return h[i].cost < h[j].cost }
 func (h PQueue) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *PQueue) Push(x any) {
-	*h = append(*h, x.(NodeWithCost))
-}
+func (h *PQueue) Push(x any)        { *h = append(*h, x.(State)) }
 
 func (h *PQueue) Pop() any {
 	old := *h
@@ -209,40 +202,27 @@ func (h *PQueue) Pop() any {
 	return x
 }
 
-func getAdjacents(cur string) []NodeWithCost {
-	node := parseState(cur)
-	next := node.nextStates()
-
-	adj := []NodeWithCost{}
-	for _, s := range next {
-		adj = append(adj, NodeWithCost{
-			node: s.key(),
-			cost: s.cost,
-		})
-	}
-	return adj
-}
-
-func dijkstra(start, stop string) (int, []NodeWithCost) {
+// dijkstra's algorithm
+func dijkstra(start, stop string) (int, []State) {
 	costs := make(map[string]int)
 	prev := make(map[string]string)
 
-	pq := PQueue{NodeWithCost{start, 0}}
+	pq := PQueue{State{start, 0}}
 	heap.Init(&pq)
 
 	for len(pq) > 0 {
-		cur := heap.Pop(&pq).(NodeWithCost)
-		node, cost := cur.node, cur.cost
+		cur := heap.Pop(&pq).(State)
+		cells, cost := cur.cells, cur.cost
 
-		if node == stop {
-			path := []NodeWithCost{{node, cost}}
-			for path[len(path)-1].node != start {
-				prevNode, ok := prev[node]
-				if !ok {
-					panic(node)
+		if cells == stop {
+			path := []State{{cells, cost}}
+			for path[len(path)-1].cells != start {
+				prevRooms, found := prev[cells]
+				if !found {
+					panic(cells)
 				}
-				node = prevNode
-				path = append(path, NodeWithCost{node, costs[prevNode]})
+				cells = prevRooms
+				path = append(path, State{cells, costs[cells]})
 			}
 			for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
 				path[i], path[j] = path[j], path[i]
@@ -250,13 +230,13 @@ func dijkstra(start, stop string) (int, []NodeWithCost) {
 			return cost, path
 		}
 
-		for _, n := range getAdjacents(node) {
-			next, edgeCost := n.node, n.cost
+		for _, n := range cur.nextStates() {
+			next, edgeCost := n.cells, n.cost
 			nextCost := cost + edgeCost
 			if c, ok := costs[next]; !ok || nextCost < c {
-				prev[next] = node
+				prev[next] = cells
 				costs[next] = nextCost
-				nextNode := NodeWithCost{next, nextCost}
+				nextNode := State{next, nextCost}
 				heap.Push(&pq, nextNode)
 			}
 		}
@@ -277,36 +257,42 @@ func parseInput(scanner *bufio.Scanner) string {
 		panic(err)
 	}
 
-	rooms := make([]rune, 23)
-	for i := range rooms {
-		rooms[i] = '.'
+	cells := make([]rune, 23)
+	for i := range cells {
+		cells[i] = '.'
 	}
 	i := 7
 	for _, col := range []int{3, 5, 7, 9} {
-		rooms[i] = []rune(lines[2])[col]
-		rooms[i+1] = []rune(lines[3])[col]
+		cells[i] = []rune(lines[2])[col]
+		cells[i+1] = []rune(lines[3])[col]
 		i += 4
 	}
-	return string(rooms)
+	return string(cells)
 }
 
-func changeInputForPart2(rooms string) string {
-	roomsMk2 := []rune(rooms)
+func changeInputForPart2(cells string) string {
+	cellsMk2 := []rune(cells)
 	addition := [4]string{"DD", "CB", "BA", "AC"}
 	for i, start := range []int{7, 11, 15, 19} {
-		roomsMk2[start+3] = []rune(rooms)[start+1]
-		roomsMk2[start+1] = []rune(addition[i])[0]
-		roomsMk2[start+2] = []rune(addition[i])[1]
+		cellsMk2[start+1] = []rune(addition[i])[0]
+		cellsMk2[start+2] = []rune(addition[i])[1]
+		cellsMk2[start+3] = []rune(cells)[start+1]
 	}
-	return string(roomsMk2)
+	return string(cellsMk2)
 }
 
 func main() {
 	start1 := parseInput(bufio.NewScanner(os.Stdin))
 	start2 := changeInputForPart2(start1)
 
-	p1cost, _ := dijkstra(start1, final1)
-	p2cost, _ := dijkstra(start2, final2)
-	fmt.Printf("Part 1: %d\n", p1cost)
-	fmt.Printf("Part 2: %d\n", p2cost)
+	startTime := time.Now()
+	p1Cost, _ := dijkstra(start1, final1)
+	p1Time := fmt.Sprint(time.Since(startTime))
+
+	startTime = time.Now()
+	p2Cost, _ := dijkstra(start2, final2)
+	p2Time := fmt.Sprint(time.Since(startTime))
+
+	fmt.Printf("Part 1: %d (%s elapsed)\n", p1Cost, p1Time)
+	fmt.Printf("Part 2: %d (%s elapsed)\n", p2Cost, p2Time)
 }
