7d91a5b5116eca32b18fbc44d6578c3e9589677c
geckoil <73975841+geckoil@users.noreply.github.com>
2023-08-17 18:38:19 +0700

    ↓

--- advent/2021/23.rs
+++ advent/2021/23.rs
@@ -1,37 +1,23 @@
-// rewriting day 19's solution for the meme (part 2 only)
+// rewriting day 19's solution for the meme
 
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
 use std::io::{self, BufRead};
 
-const COORDS: [(u8, u8); 23] = [
-    (0, 1),
-    (0, 2),
-    (0, 4),
-    (0, 6),
-    (0, 8),
-    (0, 10),
-    (0, 11),
-    (1, 3),
-    (2, 3),
-    (3, 3),
-    (4, 3),
-    (1, 5),
-    (2, 5),
-    (3, 5),
-    (4, 5),
-    (1, 7),
-    (2, 7),
-    (3, 7),
-    (4, 7),
-    (1, 9),
-    (2, 9),
-    (3, 9),
-    (4, 9),
+const CELLS_ROW: [u8; 23] = [
+    0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
 ];
 
+const CELLS_COL: [u8; 23] = [
+    1, 2, 4, 6, 8, 10, 11, 3, 3, 3, 3, 5, 5, 5, 5, 7, 7, 7, 7, 9, 9, 9, 9,
+];
+
+fn cell_coord(idx: usize) -> (u8, u8) {
+    (CELLS_ROW[idx], CELLS_COL[idx])
+}
+
 fn cell_index_at_coord(coord: (u8, u8)) -> Option<usize> {
-    COORDS.iter().position(|&c| c == coord)
+    (0..23).find(|&i| CELLS_ROW[i] == coord.0 && CELLS_COL[i] == coord.1)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -85,7 +71,11 @@ struct Move {
 }
 
 impl State {
-    fn key(&self) -> u64 {
+    // Returns the hash value of elements in the cells attribute as u64
+    // A cell can be one of 5 values (A, B, C, D, empty), the cells attribute
+    // can be represented in a base 5 system, of which the largest value
+    // (5 pow 23 - 1 ~ 1.192e16) is within u64 range
+    fn cells_key(&self) -> u64 {
         let mut res = 0;
         for &c in self.cells.iter() {
             res = 5 * res + c as u64;
@@ -93,17 +83,19 @@ impl State {
         res
     }
 
+    fn amphipod_count(&self) -> usize {
+        self.cells.iter().filter(|&&x| x != Cell::E).count()
+    }
+
     fn get_move(&self, u_idx: usize, v: (u8, u8)) -> Option<Move> {
-        assert!(u_idx < 24);
-        let u = COORDS[u_idx];
+        assert!(u_idx < 23);
+        let u = cell_coord(u_idx);
         let dist = (v.0 as i8 - u.0 as i8).abs() as u8 + (v.1 as i8 - u.1 as i8).abs() as u8;
-        let u_cell = self.cells[u_idx];
-        let cost = dist as u32 * u_cell.weight();
         if let Some(dest_idx) = cell_index_at_coord(v) {
             return Some(Move {
                 orig_idx: u_idx,
                 dest_idx,
-                cost,
+                cost: dist as u32 * self.cells[u_idx].weight(),
             });
         }
         None
@@ -113,16 +105,13 @@ impl State {
         let mut cells = self.cells.clone();
         cells[m.dest_idx] = cells[m.orig_idx];
         cells[m.orig_idx] = Cell::E;
-        State {
-            cells,
-            cost: self.cost + m.cost,
-        }
+        State { cells, cost: self.cost + m.cost }
     }
 
     fn is_hallway_segment_empty(&self, idx: usize, col_a: u8, col_b: u8) -> bool {
         let col_range = col_a.min(col_b)..=col_a.max(col_b);
         for (i, cell) in self.cells.iter().enumerate() {
-            let (row, col) = COORDS[i];
+            let (row, col) = cell_coord(i);
             if row == 0 && col_range.contains(&col) && i != idx && *cell != Cell::E {
                 return false;
             }
@@ -139,12 +128,13 @@ impl State {
 
     fn available_moves(&self) -> Vec<Move> {
         let mut moves = vec![];
+        let row_max = self.amphipod_count() as u8 / 4;
 
         'next_cell: for (i, cell) in self.cells.iter().enumerate() {
             if *cell == Cell::E {
                 continue;
             }
-            let (row, col) = COORDS[i];
+            let (row, col) = cell_coord(i);
 
             if row == 0 {
                 // Hallway to target room transition
@@ -155,7 +145,7 @@ impl State {
                 // target cell.
                 // Check if there are amphipods of other type inside the room
                 // so they won't get stuck when the amphipod moves in target room
-                for x in (1..=4).rev() {
+                for x in (1..=row_max).rev() {
                     match &self.cell_at_coord((x, dest_col)) {
                         Some(Cell::E) => {
                             dest_row = x;
@@ -163,7 +153,7 @@ impl State {
                         }
                         Some(c) if c != cell => continue 'next_cell,
                         Some(_) => (),
-                        None => panic!("how is this not a cell"),
+                        None => panic!("how is {:?} not a cell", (x, dest_col)),
                     };
                 }
 
@@ -181,7 +171,7 @@ impl State {
                     match self.cell_at_coord((x, col)) {
                         Some(Cell::E) => (),
                         Some(_) => continue 'next_cell,
-                        None => panic!("how is this not a cell"),
+                        None => panic!("how is {:?} not a cell", (x, col)),
                     };
                 }
 
@@ -190,7 +180,7 @@ impl State {
                 // move to their target rooms
                 if col == cell.target_column() {
                     let mut all_comrades = true;
-                    for x in row + 1..=4 {
+                    for x in row + 1..=row_max {
                         match &self.cell_at_coord((x, col)) {
                             Some(Cell::E) => panic!("they're not gonna bite you bro"),
                             Some(c) if c != cell => {
@@ -198,7 +188,7 @@ impl State {
                                 break;
                             }
                             Some(_) => (),
-                            None => panic!("how is this not a cell"),
+                            None => panic!("how is {:?} not a cell", (x, col)),
                         }
                     }
                     if all_comrades {
@@ -250,7 +240,6 @@ impl PartialOrd for State {
 fn dijskstra(init: &State, end: &State) -> Option<(Vec<State>, u32)> {
     let mut costs: HashMap<u64, u32> = HashMap::new();
     let mut prevs: HashMap<u64, State> = HashMap::new();
-
     let mut queue = BinaryHeap::new();
     queue.push(init.clone());
 
@@ -258,11 +247,12 @@ fn dijskstra(init: &State, end: &State) -> Option<(Vec<State>, u32)> {
         let state = queue.pop().unwrap();
         let cost = state.cost;
 
+        // end state reached, trace path and return total cost
         if state.equal_cells_with(end) {
             let mut path = vec![];
             let mut cur = state;
             loop {
-                let prev = prevs.get(&cur.key());
+                let prev = prevs.get(&cur.cells_key());
                 path.push(cur);
                 match prev {
                     Some(p) => cur = p.clone(),
@@ -276,7 +266,7 @@ fn dijskstra(init: &State, end: &State) -> Option<(Vec<State>, u32)> {
         let moves = state.available_moves();
         for m in moves.into_iter() {
             let next = state.apply_move(&m);
-            let k = next.key();
+            let k = next.cells_key();
             match costs.get(&k) {
                 Some(&c) if next.cost >= c => continue,
                 _ => {
@@ -290,29 +280,45 @@ fn dijskstra(init: &State, end: &State) -> Option<(Vec<State>, u32)> {
     None
 }
 
-fn parse_input<I>(mut lines: I) -> State
-where
-    I: Iterator<Item = String>,
-{
+fn main() {
+    let mut lines = io::stdin().lock().lines().map(Result::unwrap);
     lines.next();
-    let grid = lines
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+
+    let mut grid: Vec<Vec<_>> = lines.map(|l| l.chars().collect()).collect();
     let cells: Vec<_> = (0..23)
         .map(|i| {
-            let (x, y) = COORDS[i];
+            let (x, y) = cell_coord(i);
+            if x == 1 || x == 2 {
+                Cell::from(grid[x as usize][y as usize])
+            } else {
+                Cell::E
+            }
+        })
+        .collect();
+    let init1 = State { cells, cost: 0 };
+
+    let end1 = {
+        use Cell::*;
+        State {
+            cells: vec![
+                E, E, E, E, E, E, E, A, A, E, E, B, B, E, E, C, C, E, E, D, D, E, E,
+            ],
+            cost: 0,
+        }
+    };
+
+    grid.insert(2, "  #D#C#B#A#".chars().collect());
+    grid.insert(3, "  #D#B#A#C#".chars().collect());
+
+    let cells: Vec<_> = (0..23)
+        .map(|i| {
+            let (x, y) = cell_coord(i);
             Cell::from(grid[x as usize][y as usize])
         })
         .collect();
-    State { cells, cost: 0 }
-}
+    let init2 = State { cells, cost: 0 };
 
-fn main() {
-    // use the input that includes 2 additional lines
-    let mut lines = io::stdin().lock().lines().map(Result::unwrap);
-    let init_state = parse_input(&mut lines);
-
-    let end_state = {
+    let end2 = {
         use Cell::*;
         State {
             cells: vec![
@@ -321,5 +327,7 @@ fn main() {
             cost: 0,
         }
     };
-    println!("{:?}", dijskstra(&init_state, &end_state).unwrap().1);
+
+    println!("Part 1: {}", dijskstra(&init1, &end1).unwrap().1);
+    println!("Part 2: {}", dijskstra(&init2, &end2).unwrap().1);
 }
