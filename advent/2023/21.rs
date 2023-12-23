59bfecba9be526928ff021305c531575d4f32e49
geckoil <73975841+geckoil@users.noreply.github.com>
2023-12-23 07:19:16 +0700

    small fixes

--- advent/2023/21.rs
+++ advent/2023/21.rs
@@ -21,14 +21,14 @@ type Point = (usize, usize);
 fn part1(garden: &[Vec<char>]) -> usize {
     let n = garden.len();
     let start: Point = (n / 2, n / 2);
-    let mut to_visit: [HashSet<Point>; 2] = [HashSet::from([start]), HashSet::new()];
-    for step in 0..64 {
+    let mut to_visit = HashSet::from([start]);
+    for _ in 0..64 {
         let mut new_to_visit = HashSet::new();
-        for &(x, y) in to_visit[step % 2].iter() {
+        for &(x, y) in to_visit.iter() {
             for (nx, ny) in [
-                (x > 0).then_some((x - 1, y)),
+                x.checked_sub(1).map(|x| (x, y)),
                 (x < n - 1).then_some((x + 1, y)),
-                (y > 0).then_some((x, y - 1)),
+                y.checked_sub(1).map(|y| (x, y)),
                 (y < n - 1).then_some((x, y + 1)),
             ]
             .iter()
@@ -39,10 +39,10 @@ fn part1(garden: &[Vec<char>]) -> usize {
                 }
             }
         }
-        to_visit[(step + 1) % 2].clear();
-        to_visit[(step + 1) % 2].extend(new_to_visit);
+        to_visit.clear();
+        to_visit.extend(new_to_visit);
     }
-    to_visit[0].len()
+    to_visit.len()
 }
 
 fn fill_original_garden(garden: &[Vec<char>]) -> (usize, usize, usize) {
@@ -53,9 +53,9 @@ fn fill_original_garden(garden: &[Vec<char>]) -> (usize, usize, usize) {
         let mut new_to_visit = HashSet::new();
         for &(x, y) in to_visit[step % 2].iter() {
             for (nx, ny) in [
-                (x > 0).then_some((x - 1, y)),
+                x.checked_sub(1).map(|x| (x, y)),
                 (x < n - 1).then_some((x + 1, y)),
-                (y > 0).then_some((x, y - 1)),
+                y.checked_sub(1).map(|y| (x, y)),
                 (y < n - 1).then_some((x, y + 1)),
             ]
             .iter()
@@ -77,6 +77,7 @@ fn fill_original_garden(garden: &[Vec<char>]) -> (usize, usize, usize) {
 
 fn part2(garden: &[Vec<char>]) -> usize {
     let n = garden.len();
+    assert_eq!(n, garden[0].len());
     let (steps, even_visits, odd_visits) = fill_original_garden(garden);
     assert_eq!(steps, n / 2 * 2);
 
@@ -91,8 +92,8 @@ fn part2(garden: &[Vec<char>]) -> usize {
     //
     // Start from the original map (marked *) and expand in all 4 cardinal directions
     // (U, D, L, R) and 4 ordinal directions (UL, UR, DL, DR). The timestamp (or step)
-    // to begin filling adjacent tiles are the manhattan distance between them and
-    // the centre of (*) (because one can only take one step north, south, east, or west)
+    // to begin filling adjacent tiles are the Manhattan distances between them and
+    // the center of (*) (because one can only take one step north, south, east, or west)
     //
     // When dealing with diagonally-expanded tiles, remember to consider all tiles that are
     // not directly expanded vertically or horizontally as well
@@ -110,7 +111,7 @@ fn part2(garden: &[Vec<char>]) -> usize {
     //           :---:---:---:--
     const TARGET_STEPS: usize = 26_501_365;
 
-    // nearest point from the centre of base tile
+    // nearest point from the center of base tile
     #[rustfmt::skip]
     let fill_starts: [[Point; 3]; 3] = [
         //------L---------------------------R-------------
@@ -147,9 +148,9 @@ fn part2(garden: &[Vec<char>]) -> usize {
             let mut new_to_visit = HashSet::new();
             for &(x, y) in to_visit.iter() {
                 for (nx, ny) in [
-                    (x > 0).then_some((x - 1, y)),
+                    x.checked_sub(1).map(|x| (x, y)),
                     (x < n - 1).then_some((x + 1, y)),
-                    (y > 0).then_some((x, y - 1)),
+                    y.checked_sub(1).map(|y| (x, y)),
                     (y < n - 1).then_some((x, y + 1)),
                 ]
                 .iter()
@@ -166,7 +167,11 @@ fn part2(garden: &[Vec<char>]) -> usize {
         to_visit.len()
     };
 
-    let mut result = odd_visits;
+    let mut result = if (TARGET_STEPS - steps) % 2 == 0 {
+        even_visits
+    } else {
+        odd_visits
+    };
 
     // horizontal and vertical expansion
     for step in (n / 2 + 1..=TARGET_STEPS).step_by(n) {
